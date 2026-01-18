84a7367d7e0f39188133a2b29fa5cc3536e4066b
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2026-01-18 16:35:30 +0400

    Create GET riskassessment endpoint

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from app.db import get_db_connection
+from psycopg2.extras import RealDictCursor
 
 @app.route('/')
 def home():
@@ -25,3 +26,67 @@ def test_db_connection():
             return jsonify({ "status": "error", "message": f"Query failed: {e}" }), 500 
     else: 
         return jsonify({ "status": "error", "message": "Failed to connect to the database." }), 500
+    
+# Create GET risk history endpoint
+@app.route('/api/v1/risk-history', methods=['GET'])
+def get_risk_history():
+    patient_id = request.args.get('patient_id')
+
+    if not patient_id:
+        return jsonify({"error": "patient_id is required"}), 400
+
+    conn = get_db_connection()
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        cur.execute("""
+            SELECT *
+            FROM risk_assessments
+            WHERE patient_id = %s
+            ORDER BY created_at DESC
+            LIMIT 20;
+        """, (patient_id,))
+
+        results = cur.fetchall()
+
+        for row in results:
+            row['created_at'] = row['created_at'].isoformat()
+
+        return jsonify(results), 200
+
+    finally:
+        if conn:
+            conn.close()
+
+# Create GET risk assessment latest endpoint
+@app.route('/api/v1/risk-latest', methods=['GET'])
+def get_latest_risk():
+    patient_id = request.args.get('patient_id')
+
+    if not patient_id:
+        return jsonify({"error": "patient_id is required"}), 400
+
+    conn = get_db_connection()
+    try:
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+
+        cur.execute("""
+            SELECT *
+            FROM risk_assessments
+            WHERE patient_id = %s
+            ORDER BY created_at DESC
+            LIMIT 1;
+        """, (patient_id,))
+
+        result = cur.fetchone()
+
+        if not result:
+            return jsonify({"message": "No risk assessments found"}), 404
+
+        result['created_at'] = result['created_at'].isoformat()
+
+        return jsonify(result), 200
+
+    finally:
+        if conn:
+            conn.close()
