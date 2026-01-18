f30100d91a39fecb22386541b5daa753cc45e2e0
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2026-01-18 15:28:33 +0400

    Setup risk score & reason codes endpoint

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from app.db import get_db_connection
+import json
 
 @app.route('/')
 def home():
@@ -25,3 +26,32 @@ def test_db_connection():
             return jsonify({ "status": "error", "message": f"Query failed: {e}" }), 500 
     else: 
         return jsonify({ "status": "error", "message": "Failed to connect to the database." }), 500
+    
+# Creating POST risk scores route
+@app.route('/api/v1/risk-scores', methods=['POST'])
+def log_risk():
+    data = request.json
+    conn = get_db_connection()
+    try:
+        cur = conn.cursor()
+        
+        # Convert Python list to JSON string for the database
+        reason_codes_json = json.dumps(data.get('reason_codes', []))
+        
+        query = """
+            INSERT INTO risk_assessments 
+            (patient_id, risk_score, risk_label, reason_codes) 
+            VALUES (%s, %s, %s, %s);
+        """
+        cur.execute(query, (
+            data['patient_id'],
+            data['risk_score'],
+            data['risk_label'], # e.g., 'High', 'Medium'
+            reason_codes_json
+        ))
+        conn.commit()
+        return jsonify({"message": "Risk assessment saved"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    finally:
+        if conn: conn.close()
