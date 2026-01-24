743206bfe9223a0e6442d61f0ec4db94b2e6ad75
Maw1z <mohdmawiz11@gmail.com>
2026-01-24 07:09:39 +0400

    Uploading backend to vercel

--- server/app/routes.py
+++ server/app/routes.py
@@ -3,7 +3,6 @@ from flask import json, jsonify, request
 from app.db import get_db_connection
 from psycopg2.extras import RealDictCursor
 import logging
-import uuid
 
 @app.route('/')
 def home():
@@ -11,23 +10,6 @@ def home():
         "status": "success",
         "message": "Welcome to the SHIELD Doctor API!"
         }), 200
-
-@app.route('/users')
-def test_db_connection():
-    conn = get_db_connection() 
-    if conn: 
-        cur = conn.cursor() 
-        try: 
-            cur.execute("SELECT * FROM users;") 
-            result = cur.fetchone() 
-            cur.close() 
-            conn.close() 
-            
-            return jsonify({ "status": "success", "message": f"Users retrieved successfully! First row: {result}" }), 200 
-        except Exception as e: 
-            return jsonify({ "status": "error", "message": f"Query failed: {e}" }), 500 
-    else: 
-        return jsonify({ "status": "error", "message": "Failed to connect to the database." }), 500
 
 # Creating POST log vitals route
 @app.route('/api/v1/log_vitals', methods=['POST'])
