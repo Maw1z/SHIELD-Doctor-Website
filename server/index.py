b16e2dcc073efc6f64615ac37a64a07a33cdd762
Maw1z <mohdmawiz11@gmail.com>
2026-01-24 07:21:56 +0400

    Update index.py

--- server/index.py
+++ server/index.py
@@ -3,9 +3,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from app import create_app
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
     app.run()
